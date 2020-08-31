55b0020dc9dc920c55f877c2b93c1ede60d31b57
Akshay Joshi <akshayjoshi56@gmail.com>
2020-08-31 08:07:45 +0200

    Refactoring the code

--- SLR.py
+++ SLR.py
@@ -7,46 +7,65 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 from sklearn.metrics.pairwise import cosine_similarity
 
-pfile = open("phoneme_embeddings.tsv", encoding="utf-8")
-reader = csv.reader(pfile, delimiter='\t')
-phoneme_dict = {}
-for row in reader:
-    phoneme_dict[row[0]] = row[1:]
+class SLR:
+    def __init__(self):
+        self.file_path = "phoneme_embeddings.tsv"
 
-pairwise_sim_results = []
-counter = 0
-for i in phoneme_dict:
-    counter += 1
-    outer = np.array(phoneme_dict[i]).reshape(1, -1)
-    for j in phoneme_dict:
-        inner = np.array(phoneme_dict[j]).reshape(1, -1)
-        similarity = cosine_similarity(outer, inner)
-        res = list(itertools.chain.from_iterable(similarity))
-        pairwise_sim_results.append([i, j, res[0]])
-    # if counter == 2:
-    #     break
+    # Generate a confusion matrix/heat map of the cosine similariities of phoneme vector pairs
+    def plotHeatMap(self, final_sim_vectors, final_phoneme_labels):
+        fig, ax = plt.subplots()
+        im = ax.imshow(final_sim_vectors)
+        ax.set_xticks(np.arange(len(final_phoneme_labels)))
+        ax.set_yticks(np.arange(len(final_phoneme_labels)))
+        ax.set_xticklabels(final_phoneme_labels)
+        ax.set_yticklabels(final_phoneme_labels)
+        ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+        cbar = ax.figure.colorbar(im, ax=ax)
+        cbar.ax.set_ylabel("Heatmap of Cosine Similarities of Phoneme Vectors", rotation=-90, va="bottom")
+        fig.tight_layout()
+        plt.show()
+    
 
-pprint(pairwise_sim_results)
-print(len(pairwise_sim_results))
+    # Generate a dictionary with keys as the phonemes and their corresponding row vector containing 
+    # cosine similarities against other phoneme vectors as value 
+    def pairwiseSimilarityDictionary(self):
+        pfile = open(self.file_path, encoding="utf-8")
+        reader = csv.reader(pfile, delimiter='\t')
+        phoneme_dict = {}
+        pairwise_sim_results = []
+        for row in reader:
+            phoneme_dict[row[0]] = row[1:]
 
-similarity_dictionary = defaultdict(list)
-for index, value in enumerate(pairwise_sim_results, start = 1):
-    similarity_dictionary[value[0]].append(value[2])
+        for i in phoneme_dict:
+            outer = np.array(phoneme_dict[i]).reshape(1, -1)
+            for j in phoneme_dict:
+                inner = np.array(phoneme_dict[j]).reshape(1, -1)
+                similarity = cosine_similarity(outer, inner)
+                res = list(itertools.chain.from_iterable(similarity))
+                pairwise_sim_results.append([i, j, res[0]])
 
-final_sim_vectors = [value for key, value in similarity_dictionary.items()]
-final_phoneme_labels = [key for key, value in similarity_dictionary.items()]
-#print(len(final_phoneme_labels), final_phoneme_labels)
+        #pprint(pairwise_sim_results)
+        print(f"Total number of phoneme pairs: {len(pairwise_sim_results)}")
 
-final_sim_vectors = np.asarray(final_sim_vectors).reshape(50, 50)
+        similarity_dictionary = defaultdict(list)
+        for index, value in enumerate(pairwise_sim_results, start = 1):
+            similarity_dictionary[value[0]].append(value[2])
+        
+        return similarity_dictionary
+    
+    # Return a final reshaped 2D numpy array consisting of pairwise cosine similarities of phoneme embeddings 
+    # extracted from cosine similarity dictionary
+    def finalPhonemeSimilaritiesList(self, similarity_dictionary):
+        final_phoneme_labels = [key for key, value in similarity_dictionary.items()]
 
-fig, ax = plt.subplots()
-im = ax.imshow(final_sim_vectors)
-ax.set_xticks(np.arange(len(final_phoneme_labels)))
-ax.set_yticks(np.arange(len(final_phoneme_labels)))
-ax.set_xticklabels(final_phoneme_labels)
-ax.set_yticklabels(final_phoneme_labels)
-ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
-cbar = ax.figure.colorbar(im, ax=ax)
-cbar.ax.set_ylabel("Heatmap of Cosine Similarities of Phoneme Vectors", rotation=-90, va="bottom")
-fig.tight_layout()
-plt.show()
+        # Converting the final similarity vector list into numpy array for easier reshaping into a 2D matrix
+        final_sim_vectors = [value for key, value in similarity_dictionary.items()]
+        final_sim_vectors = np.asarray(final_sim_vectors).reshape(50, 50)
+
+        return final_sim_vectors, final_phoneme_labels
+
+if __name__ == "__main__":
+    task = SLR()
+    similarity_dictionary = task.pairwiseSimilarityDictionary()
+    final_sim_vectors, final_phoneme_labels = task.finalPhonemeSimilaritiesList(similarity_dictionary)
+    task.plotHeatMap(final_sim_vectors, final_phoneme_labels)
